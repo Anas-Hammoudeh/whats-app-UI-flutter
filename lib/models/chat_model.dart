be808b3611dd54f2b60c578c1a54e118cdceca2c
Anas-Hammoudeh <anashammoudeh44@gmail.com>
2021-11-04 18:50:26 +0200

    first commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "mohammad ali",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "jgon smith",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Mike maloine",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "miachle",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "mike Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
